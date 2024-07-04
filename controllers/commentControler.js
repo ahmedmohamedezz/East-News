a68f3bb12738814c728089dc8a74c90426f2c256
Ahmed Ezz <ahmedezzeldin2002@gmail.com>
2024-07-05 00:07:46 +0300

    Changed the summarization package

--- controllers/commentControler.js
+++ controllers/commentControler.js
@@ -2,16 +2,15 @@ const User = require("../models/userModel");
 const comments = require("../models/commentsModel");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { ObjectId } = require('mongodb');
+const { ObjectId } = require("mongodb");
 
 const getcomment = async (req, res) => {
   const { articleID } = req.body;
   const query = { articleID: articleID };
   try {
     const comment = await comments.find(query);
-    return res.status(200).json({comment});
-  }
-  catch (error) {
+    return res.status(200).json({ comment });
+  } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
@@ -21,14 +20,20 @@ const addcomment = async (req, res) => {
   const { authorization } = req.headers;
   // authorization should be 'Bearer token'
   const token = authorization.split(" ")[1];
-  const { text,articleID } = req.body;
+  const { text, articleID } = req.body;
   try {
     const { _id } = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findOne({ _id });
     const userName = user.userName;
     const authorID = _id;
-    const comment = await comments.create({ text, publishDate ,userName , authorID, articleID});
-    res.status(200).json({comment});
+    const comment = await comments.create({
+      text,
+      publishDate,
+      userName,
+      authorID,
+      articleID,
+    });
+    res.status(200).json({ comment });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -42,17 +47,16 @@ const editcomment = async (req, res) => {
   try {
     const { _id } = jwt.verify(token, process.env.JWT_SECRET);
     const comment = await comments.findOne({ _id: commentID });
-    if (comment&&comment.authorID.equals(new ObjectId(_id))) {
+    if (comment && comment.authorID.equals(new ObjectId(_id))) {
       const filter = { _id: new ObjectId(commentID) };
       const updateDoc = {
         $set: {
-        text: text
+          text: text,
         },
       };
       const result = await comments.updateOne(filter, updateDoc);
-      res.status(200).json({result});
-    }
-    else {
+      res.status(200).json({ result });
+    } else {
       res.status(400).json("Can not edit");
     }
   } catch (error) {
@@ -64,15 +68,14 @@ const deletecomment = async (req, res) => {
   const { authorization } = req.headers;
   // authorization should be 'Bearer token'
   const token = authorization.split(" ")[1];
-  const {commentID } = req.body;
+  const { commentID } = req.body;
   try {
     const { _id } = jwt.verify(token, process.env.JWT_SECRET);
     const comment = await comments.find({ _id: commentID });
-    if (comment&&comment[0].authorID.equals(new ObjectId(_id))) {
+    if (comment && comment[0].authorID.equals(new ObjectId(_id))) {
       const result = await comments.deleteOne(new ObjectId(commentID));
-      res.status(200).json({result});
-    }
-    else {
+      res.status(200).json({ result });
+    } else {
       res.status(400).json("Can not edit");
     }
   } catch (error) {
@@ -86,4 +89,3 @@ module.exports = {
   editcomment,
   deletecomment,
 };
-
--- controllers/newsController.js
+++ controllers/newsController.js
@@ -7,10 +7,17 @@ const Saved = require("../models/savedModel");
 
 const mongoose = require("mongoose");
 
-const smmry = require("smmry")({
-  SM_API_KEY: process.env.SM_API_KEY,
-  SM_LENGTH: 10,
+const NLPCloudClient = require("nlpcloud");
+const client = new NLPCloudClient({
+  model: "bart-large-cnn",
+  token: "9d15a79b99ad0ecafff7aafe0c34a1c28378180f",
+  gpu: true,
 });
+
+// const smmry = require("smmry")({
+//   SM_API_KEY: process.env.SM_API_KEY,
+//   SM_LENGTH: 10,
+// });
 
 const getNews = async (req, res) => {
   // for filtering upon language, category, country
@@ -78,15 +85,30 @@ const summarize = async (req, res) => {
     return res.status(400).json({ message: "article content is required" });
   }
 
-  smmry
-    .summarizeText(text)
-    .then((data) => {
-      return res.status(200).json({ summary: data.sm_api_content });
+  client
+    .summarization({
+      text: text,
+      size: "large",
     })
-    .catch((err) => {
-      // console.error(err);
-      return res.status(500).json({ err });
+    .then(function (response) {
+      return res.status(200).json({ summary: response.data.summary_text });
+    })
+    .catch(function (err) {
+      return res
+        .status(500)
+        .json({ err: err.response.data.detail, status: err.response.status });
     });
+  // smmry
+  //   .summarizeText(text)
+  //   .then((data) => {
+  //     const sentences = text.split(".").length;
+  //     // return res.status(200).json({ summary: data.sm_api_content });
+  //     return res.status(200).json({ data, sentences });
+  //   })
+  //   .catch((err) => {
+  //     // console.error(err);
+  //     return res.status(500).json({ err });
+  //   });
 };
 
 // check that data exists, and it's valid
