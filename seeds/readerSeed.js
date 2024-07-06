7e1c5d8e305cabc47efc155ef783ea169705721e
Ahmed Ezz <ahmedezzeldin2002@gmail.com>
2024-07-06 23:18:02 +0300

    push

--- seeds/readerSeed.js
+++ seeds/readerSeed.js
@@ -1,12 +1,12 @@
 const say = require("say");
 
 const convertToMp3 = (text, filename, callback) => {
-    say.export(text, "", 0.75, filename, function(err) {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, filename);
-    });
+  say.export(text, "", 0.75, filename, function (err) {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, filename);
+  });
 };
 
-module.exports=convertToMp3;
+module.exports = convertToMp3;
